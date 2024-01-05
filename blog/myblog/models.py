ded62428d5258d031b030b98d21467aba7a48945
nickAnhel <127442031+nickAnhel@users.noreply.github.com>
2024-01-05 19:32:28 +0300

    first commit

---
+++ blog/myblog/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.db.models.query import QuerySet
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class PublishedManager(models.Manager):
+    def get_queryset(self) -> QuerySet:
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
+class Post(models.Model):
+    # Publication status
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    # Data fields
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    body = models.TextField()
+
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+
+    # Managers
+    objects = models.Manager() # Default manager
+    published = PublishedManager() # Manager for published posts
+
+    # Meta data
+    class Meta:
+        ordering = ['-publish']
+        indexes = [models.Index(fields=['-publish'])]
+
+
+    def __str__(self) -> str:
+        return self.title
+
+
+    def get_absolute_url(self) -> str:
+        return reverse('myblog:post_detail', args=[self.publish.year,
+                                                   self.publish.month,
+                                                   self.publish.day,
+                                                   self.slug])
---
+++ blog/myblog/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest, HttpResponse, Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+from .models import Post
+from .forms import EmailPostForm
+
+
+# View for rendering a list of posts
+# Function
+"""
+def post_list(request: HttpRequest) -> HttpResponse:
+    post_list = Post.published.all()
+
+    # Pagination
+    paginator = Paginator(post_list, 2)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages) # Return the last page if page index out of range
+    except PageNotAnInteger:
+        posts = paginator.page(1) # Return the first page if page number not an integer
+
+    return render(request, 'myblog/post/list.html', {'posts': posts})
+"""
+
+# Class
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'myblog/post/list.html'
+
+
+# View for rendering a single post page
+def post_detail(request: HttpRequest, year: int, month: int, day: int, post) -> HttpResponse:
+    """
+    # Solution with try/except
+    try:
+        post = Post.published.get(publish__year=year, publish__month=month, publish__day=day, slug=post)
+    except Post.DoesNotExist:
+        raise Http404('Post not found')
+    """
+
+    # Solution with shortcut function (MVC)
+    post = get_object_or_404(Post,
+                            status=Post.Status.PUBLISHED,
+                            slug=post,
+                            publish__year=year,
+                            publish__month=month,
+                            publish__day=day)
+
+    return render(request, 'myblog/post/detail.html', {'post': post})
+
+
+# View for rensering email form
+def post_share(request: HttpRequest, post_id: int) -> HttpResponse:
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             id=post_id)
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data # cd - dictonary type {"form_field": data}
+            # ...
+
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'myblog/post/share.html', {'post': post, 'form': form})
